a75de386496f3f0e00a91d62643ac7ba5d5ef02e
Alex Meislich <alex.meislich@gmail.com>
2022-11-30 22:11:54 -0800

    clarified day 1

--- solutions/day1.py
+++ solutions/day1.py
@@ -1,14 +1,12 @@
 f = open("../inputs/day1.txt")
-food = [elf.split("\n") for elf in [elf for elf in f.read().split("\n\n")]]
-cals = [[int(cal) for cal in elf] for elf in food]
-def part1():
-    return max([sum(elf) for elf in cals])
+# get a list of calories for each elf:
+elfCalsStr: [[str]] = [elf.split("\n") for elf in f.read().split("\n\n")]
+elfCals: [[int]] = [[int(cal) for cal in elf] for elf in elfCalsStr]
 
-def part2():
-    elvesByCals = [sum(elf) for elf in cals]
-    elvesByCals.sort(reverse=True)
-    return elvesByCals[0] + elvesByCals[1] + elvesByCals[2]
+# sum the calories for the top n elves
+def sumTopElves(n):
+    topn = sorted([sum(cals) for cals in elfCals])[-n:]
+    return sum(topn)
 
-print("part1: " + str(part1()))
-print("part2: " + str(part2()))
 
+print("part1: " + str(sumTopElves(1)) + "\npart2: " + str(sumTopElves(3)))
